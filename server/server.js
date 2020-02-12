d7bc389fe7325b94b7f48d33b1f53cc9a1515e69
Miika Leinonen <miika.leinonen@aalto.fi>
2020-02-12 19:44:36 +0200

    Ready to try deploying

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+
+const PORT = process.env.PORT || 3000
+
+const publicPath = path.join(__dirname, '..', 'build')
+
+app.use(express.static(publicPath))
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
