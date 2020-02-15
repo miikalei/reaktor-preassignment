9f45a4c3e10fc6fefe7e0ef915c0082b6e9dc51d
Miika Leinonen <miika.leinonen@aalto.fi>
2020-02-15 15:46:28 +0200

    Add reading  user file or the mock data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [ textData, setTextData ] = useState("Default text")
+
+  const loadDefaultFile = () => {
+    fetch('/data/defaultdata')
+      .then((r) => r.text())
+      .then((data) => setTextData(data))
+  }
+
+  const loadCustomFile = (file) => {
+    const reader = new FileReader()
+    reader.onload = (event) => {
+      setTextData(event.target.result)
+    }
+    reader.readAsText(file)
+  }
+
   return (
     <div className="App">
-      Heipä hei
+      <input type="file" onChange={(event) => loadCustomFile(event.target.files[0])}/>
+      <button onClick={ () => loadDefaultFile() }>Load default file</button>
+      <div id="textarea">
+        { textData }
+      </div>
     </div>
   );
 }
